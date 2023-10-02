738b302a0d4b0a64b8107074b67ba4812cd4cd5d
Isaac <isaac.correia@outlook.com.br>
2023-10-01 23:26:08 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const repositores = document.querySelector('#cartoes-projeto')
+
+function getApiGitHub(){
+  fetch('https://api.github.com/users/IsaacCSantos/repos').then(async res => {
+    if(!res.ok){
+      throw new Error(res.status);
+    }
+
+    var data = await res.json();
+
+    data.map( item => {
+      let project = document.createElement('div');
+
+      project.innerHTML = `
+        <div id="cartao-projeto" class="card">
+          <div id="projeto-titulo">
+            <div id="titulo">
+                <img src="./assets/folder.svg"/> 
+                <a href="${item.html_url}" target="_blank">${ item.name}</a>
+            </div>
+            <div class="data">${ Intl.DateTimeFormat('pt-BR').format(new Date(item.created_at))}</div>
+          </div>
+          <p>${item.description}</p>
+          <div id="cartao-projeto-info">
+              <div id="cartao-projeto-info-icon">
+                  <img src="./assets/star.svg"/> 
+                  <p>${item.stargazers_count}</p> 
+                  <img src="./assets/git-branch.svg"/> 
+                  <p>${item.forks_count}</p>
+              </div>
+              <div id="linguagem">
+                  <img class="circle" src="./assets/Ellipse 2.svg"/>
+                  <p>${item.language}</p>
+              </div>
+          </div>
+        </div>
+      `
+
+      repositores.appendChild(project);
+    })
+
+  }).catch(e => console.log(e))
+}
+
+getApiGitHub()
